7a63fb86370696ef0d8abc9121a098b971b10443
Takasur Azeem <takasurazeem@gmail.com>
2024-03-25 15:59:42 +0500

    Initial Commit

---
+++ iTour/iTourApp.swift
@@ -0,0 +1,17 @@
+//
+//  iTourApp.swift
+//  iTour
+//
+//  Created by Takasur Home on 25/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct iTourApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
