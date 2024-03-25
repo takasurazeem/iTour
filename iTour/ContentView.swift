3d937329b2648842f35fe6d02d3bb8c0c6e6ce42
Takasur Azeem <takasurazeem@gmail.com>
2024-03-25 16:23:58 +0500

    Querying SwiftData objects
    SwiftData Starter Project 3/8

--- iTour/ContentView.swift
+++ iTour/ContentView.swift
@@ -6,16 +6,57 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct ContentView: View {
+    @Query var destinations: [Destination]
+    @Environment(\.modelContext) var modelContext
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List {
+                ForEach(destinations) { destination in
+                    VStack(alignment: .leading) {
+                        Text(destination.name)
+                            .font(.headline)
+                        
+                        Text(
+                            destination.date.formatted(
+                                date: .long,
+                                time: .shortened
+                            )
+                        )
+                    }
+                }
+            }
+            .navigationTitle("iTour")
+            .toolbar {
+                Button("Add Samples", action: addSamples)
+            }
         }
-        .padding()
+    }
+    
+    func addSamples() {
+        let Palestine = Destination(
+            name: "Palestine",
+            details: "Qibla e Awwal and 3rd holiest site in Islam"
+        )
+        let Makkah = Destination(
+            name: "Makkah 🕋",
+            details: "Qibla and the holiest site in Islam",
+            priority: 1
+        )
+        let Madinah = Destination(
+            name: "Madinah",
+            details: "Prophet Muhammad's Masjid and the second holiest site in Islam",
+            priority: 1
+        )
+        
+        if destinations.isEmpty {
+            modelContext.insert(Palestine)
+            modelContext.insert(Makkah)
+            modelContext.insert(Madinah)
+        }
     }
 }
 
